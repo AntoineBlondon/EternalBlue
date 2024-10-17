1fbdf615b87c056c13ecac7ef27a471c7508dfcc
AntoineBlondon <antoine@blondon.fr>
2024-10-17 14:12:15 +0200

    added header to script

--- scripts/convert_md_to_html.py
+++ scripts/convert_md_to_html.py
@@ -1,6 +1,25 @@
 import os
 import markdown
 
+
+
+header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hi!</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+"""
+
+footer = """
+</body>
+</html>
+"""
+
+
+
 input_folder = 'markdown'
 output_folder = 'html'
 os.makedirs(input_folder, exist_ok=True)
@@ -13,6 +32,7 @@ for root, dirs, files in os.walk(input_folder):
             with open(filepath, 'r') as f:
                 text = f.read().replace(".md)", ".html)")
                 html = markdown.markdown(text)
+                html = header + html + footer
 
             relative_path = os.path.relpath(filepath, input_folder)
             output_path = os.path.join(output_folder, relative_path.replace('.md', '.html'))
