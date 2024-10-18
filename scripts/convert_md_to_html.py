b28baff7cebbca0a7539de5c900a1efd7c754e2a
AntoineBlondon <antoine@blondon.fr>
2024-10-18 12:23:11 +0200

    made website responsive for mobile devices

--- scripts/convert_md_to_html.py
+++ scripts/convert_md_to_html.py
@@ -11,6 +11,7 @@ def header(title, css_path):
 <head>
     <title>{title}</title>
     <link rel="stylesheet" href="{css_path}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 """
