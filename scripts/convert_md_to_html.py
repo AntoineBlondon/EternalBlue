a91553bc9081a9c591c572eb1b08f0fe73fec306
AntoineBlondon <antoine@blondon.fr>
2025-05-27 21:35:38 +0200

    trying stuff

--- scripts/convert_md_to_html.py
+++ scripts/convert_md_to_html.py
@@ -17,9 +17,9 @@ def header(title, css_path):
 <body>
     <header>
         <div class="navbar">
-            <a href="index.html">home</a>
-            <a href="projects.html">projects</a>
-            <a href="about.html">about</a>
+            <a href="./">home</a>
+            <a href="./projects">projects</a>
+            <a href="./about">about</a>
         </div>
     </header>
 """
@@ -37,9 +37,28 @@ css_folder =  'markdown/css'
 os.makedirs(input_folder, exist_ok=True)
 os.makedirs(output_folder, exist_ok=True)
 
+
 for root, dirs, files in os.walk(input_folder):
     for filename in files:
-        if filename.endswith('.md'):
+        if filename.endswith('.md') and not filename.endswith("index.md"):
+            filepath = os.path.join(root, filename)
+            css_path = os.path.relpath(os.path.join(css_folder, 'style.css'), root)
+            with open(filepath, 'r') as f:
+                text = f.read().replace(".md)", "/)")
+                title = text.split('\n')[0].replace('# ', '')
+                html = markdown.markdown(text, extensions=['attr_list'])
+                html = header(title, css_path) + html + footer
+
+            relative_path = os.path.relpath(filepath, input_folder)
+            base_name = os.path.splitext(relative_path)[0]  # no extension
+            output_dir = os.path.join(output_folder, base_name)
+            os.makedirs(output_dir, exist_ok=True)
+            output_path = os.path.join(output_dir, 'index.html')
+
+            with open(output_path, 'w') as f:
+                f.write(html)
+
+        elif filename.endswith("index.md"):
             filepath = os.path.join(root, filename)
             css_path = os.path.relpath(os.path.join(css_folder, 'style.css'), root)
             with open(filepath, 'r') as f:
@@ -53,8 +72,10 @@ for root, dirs, files in os.walk(input_folder):
             os.makedirs(os.path.dirname(output_path), exist_ok=True)
             with open(output_path, 'w') as f:
                 f.write(html)
-        elif filename.endswith('.png'): # Copy image
+
+        elif filename.endswith('.png'):
             filepath = os.path.join(root, filename)
             relative_path = os.path.relpath(filepath, input_folder)
             output_path = os.path.join(output_folder, relative_path)
+            os.makedirs(os.path.dirname(output_path), exist_ok=True)
             shutil.copyfile(filepath, output_path)
