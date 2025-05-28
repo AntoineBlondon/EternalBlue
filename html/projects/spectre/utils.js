5cbe701473fae711801b05caaa47f04e346cdcb1
AntoineBlondon <antoine@blondon.fr>
2025-05-28 13:04:56 +0200

    debugging

--- html/projects/spectre/utils.js
+++ html/projects/spectre/utils.js
@@ -32,16 +32,14 @@ function freqToY(freq, canvasHeight, minFreq, maxFreq, scale = 'log') {
  * @returns {number} Frequency in Hz
  */
 function yToFreq(y, canvasHeight, minFreq, maxFreq, scale = 'log') {
-    console.log(y, canvasHeight, minFreq, maxFreq, scale);
-  const norm = 1 - (y / canvasHeight);
-  if (scale === 'log') {
-    const logMin = Math.log10(minFreq);
-    const logMax = Math.log10(maxFreq);
-    const logFreq = logMin + norm * (logMax - logMin);
-    return Math.pow(10, logFreq);
-  } else {
-    return minFreq + norm * (maxFreq - minFreq);
-  }
+    if (scale === 'log') {
+        const logMin = Math.log10(minFreq);
+        const logMax = Math.log10(maxFreq);
+        const logFreq = logMax - (y / canvasHeight) * (logMax - logMin);
+        return Math.pow(10, logFreq);
+    } else {
+        return maxFreq * (1 - y / height);
+    }
 }
 
 /**
